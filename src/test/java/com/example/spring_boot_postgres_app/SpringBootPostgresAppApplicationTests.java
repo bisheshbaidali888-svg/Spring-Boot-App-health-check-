48565dc7f2bada562d2ddbb1cc537877fccce031
BISHESH BAIDALI <bisheshbaidali888@gmail.com>
2026-06-12 12:27:55 +0000

    spring-boot-health-endpoint

---
+++ src/test/java/com/example/spring_boot_postgres_app/SpringBootPostgresAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_postgres_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgresAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
